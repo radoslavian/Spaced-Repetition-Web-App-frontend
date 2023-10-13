04ce57b07efd81eb615a4e9a5bffc47e91f44759
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-10-13 17:58:43 +0200

    Added spinner indicating data loading in GradesDistributionPage.

--- src/components/GradesDistributionPage.js
+++ src/components/GradesDistributionPage.js
@@ -2,11 +2,13 @@ import {  useEffect, useState } from "react";
 import { GradesDistributionChart } from "./DistributionCharts";
 import { useApi } from "../contexts/ApiProvider";
 import { useUser } from "../contexts/UserProvider";
+import { Spin } from "antd";
 
 export default function GradesDistributionPage() {
     const api = useApi();
     const { user } = useUser();
     const [data, setData] = useState({});
+    const [loadingData, setLoadingData] = useState(false);
 
     // compare this and above portion with CardsDistributionPage
     // shouldn't it all go into it' own hook?
@@ -14,11 +16,18 @@ export default function GradesDistributionPage() {
     useEffect(() => {
         (async () => {
             if (!Boolean(user)) return;
+            setLoadingData(true);
             const url = `/users/${user.id}/cards/distribution/grades/`;
             const response = await api.get(url);
             if(Boolean(response)) setData(response);
+            setLoadingData(false);
         })();
     }, [user, api]);
 
-    return <GradesDistributionChart chartData={data}/>;
+    return (
+        <Spin spinning={loadingData}
+              delay={500}>
+          <GradesDistributionChart chartData={data}/>
+        </Spin>
+    );
 }
