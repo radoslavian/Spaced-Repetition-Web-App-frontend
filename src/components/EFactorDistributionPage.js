15ed94cc0fd87123e624993e6c033f7fc129ef00
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-10-13 18:02:43 +0200

    Added spinner indicating data loading into EFactorDistributionPage.

--- src/components/EFactorDistributionPage.js
+++ src/components/EFactorDistributionPage.js
@@ -2,11 +2,13 @@ import {  useEffect, useState } from "react";
 import { EFactorDistributionChart } from "./DistributionCharts";
 import { useApi } from "../contexts/ApiProvider";
 import { useUser } from "../contexts/UserProvider";
+import { Spin } from "antd";
 
 export default function EFactorDistributionPage() {
     const api = useApi();
     const { user } = useUser();
     const [data, setData] = useState([]);
+    const [loadingData, setLoadingData] = useState(false);
 
     // compare this and above portion with GradesDistributionPage
     // shouldn't it all go into it' own hook?
@@ -14,12 +16,19 @@ export default function EFactorDistributionPage() {
     // data passed into useState hook.
     useEffect(() => {
         (async () => {
+            setLoadingData(true);
             if (!Boolean(user)) return;
             const url = `/users/${user.id}/cards/distribution/e-factor/`;
             const response = await api.get(url);
             if(Boolean(response)) setData(response);
+            setLoadingData(false);
         })();
     }, [user, api]);
 
-    return <EFactorDistributionChart chartData={data}/>;
+    return (
+        <Spin spinning={loadingData}
+              delay={500}>
+          <EFactorDistributionChart chartData={data}/>
+        </Spin>
+    );
 }
