aae08920a53f903064343c07e179f39150a0b77b
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-10-16 21:45:00 +0200

    Added warning about component description.

--- src/components/Suspense.js
+++ src/components/Suspense.js
@@ -1,4 +1,10 @@
 import React from "react";
+// Custom component for conditional rendering - to remove
+// clutter from code.
+// Warning: be aware that 'children' are executed anyway
+// (no matter if 'displayChildren' is true or false)
+// so in certain scenarios it is safer to use
+// ternary or && operator.
 
 export default function Suspense(
     {children, fallback, displayChildren = false}) {
