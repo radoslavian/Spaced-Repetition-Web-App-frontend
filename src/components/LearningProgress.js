920c43f118284f6fae17cca6ebb7cbd284c8d7ac
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-11-09 14:06:25 +0100

    Improved component looks on small screens.

--- src/components/LearningProgress.js
+++ src/components/LearningProgress.js
@@ -5,7 +5,16 @@ export default function LearningProgress(
     {scheduled = 0, cramQueue=0, queued=0}) {
     return(
         <Descriptions data-testid="learning-progress-indicator"
-                      size="small">
+                      size="small"
+                      column={{
+                          xs: 3,
+                          sm: 3,
+                          md: 3,
+                          lg: 3,
+                          xl: 3,
+                          xxl: 3,
+                      }}
+        >
         <Item label="Scheduled">
           {scheduled}
         </Item>
