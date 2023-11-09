85be42fcc394a1cad61cf8208fac8d97fa17acc9
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-11-09 23:46:25 +0100

    Removed CardsMenu

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -1,8 +1,16 @@
 import { Card } from "antd";
+import CardsMenu from "./CardsMenu";
 
 export default function MainDisplay({testId, title, children}) {
+    const mainTitle = (
+        <>
+          <CardsMenu/>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <span>{ title }</span>
+        </>
+    );
     return (
-        <Card title={title}
+        <Card title={mainTitle}
               size="large"
               data-testid={testId}
               type="inner"
