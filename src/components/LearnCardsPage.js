4c60e85400365fe1f37a022467510fbbcc5929f2
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-11-13 16:14:36 +0100

    Removed button for showing the sider up.
    CardsMenu has been moved into MainDisplay.js (component for viewing and rating cards).
    Updated CardsBrowserModal props (along with changes within component).

--- src/components/LearnCardsPage.js
+++ src/components/LearnCardsPage.js
@@ -1,53 +1,37 @@
 import { Layout, Space } from "antd";
 import { useState } from "react";
-import { RightOutlined, LeftOutlined } from "@ant-design/icons";
 import CardCategoryBrowser from "./CardCategoryBrowser";
 import CardDetails from "./CardDetails";
 import CardsSelector from "./CardsSelector";
-import CardsMenu from "./CardsMenu";
+import CardsBrowserModal from "./CardsBrowserModal";
+import { useCards } from "../contexts/CardsProvider";
 
 const { Content, Sider } = Layout;
 
-export default function LearnCardsPage() {
+export default function LearnCardsPage({set_cardBody_visible}) {
     const [currentCard, setCurrentCard] = useState(undefined);
     const [cardBody_visible, setCardBody_visible] = useState(true);
-    const [collapsed, setCollapsed] = useState(false);
-    const handleCollapsed = () => setCollapsed(!collapsed);
-
-    const TriggerButton = () => (
-        <div style={{ position: "absolute",
-                      zIndex: 7,
-                      cursor: "pointer",
-                      paddingTop: "20px",
-                      paddingLeft: "5px" }}
-             onClick={handleCollapsed}>
-          { collapsed ?
-            <RightOutlined title="Expand sider"/>
-            :
-            <LeftOutlined title="Hide sider"/> }
-        </div>
-    );
+    const allCards = useCards().all;
 
     return (
         <Layout>
           <Sider width={280}
                  theme="light"
-                 breakpoint="md"
                  trigger={null}
-                 collapsedWidth={0}
-                 collapsed={collapsed}
-                 onCollapse={handleCollapsed}>
+        /* Update styling (index.css) if 
+         * changing breakpoint: */
+                 breakpoint="md"
+                 collapsedWidth={0}>
             <Space direction="vertical"
                    size="small"
                    style={{ marginTop: "15px" }}>
-              <CardsMenu/>
-              <CardCategoryBrowser
-                set_cardBody_visible={setCardBody_visible}/>
+              <CardCategoryBrowser/>
+              <CardsBrowserModal cards={allCards}
+                                 set_cardBody_visible={setCardBody_visible}/>
               <CardDetails card={currentCard}/>
             </Space>
           </Sider>
           <Content style={{backgroundColor: "white"}}>
-            <TriggerButton/>
             <CardsSelector
               setCurrentCard={setCurrentCard}
               displayCardBody={cardBody_visible}/>
