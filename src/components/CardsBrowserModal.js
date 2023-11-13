1abb7fe92194c14b9abc8f85a4762406197717a3
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-11-13 16:05:15 +0100

    Moved logic for Modal inside the component.

--- src/components/CardsBrowserModal.js
+++ src/components/CardsBrowserModal.js
@@ -1,29 +1,48 @@
-import { Modal } from "antd";
-import { Button } from "antd";
+import { useState } from "react";
+import { Modal, Button } from "antd";
+import AllCardsBrowser from "./AllCardsBrowser.js";
+import CardsBrowserTitle from "./CardsBrowserTitle.js";
 
-export default function CardsBrowser(
-    {title, children, isOpen, closeModal, onClose}) {
-
+export default function AllCardsBrowserModal(
+    {cards, set_cardBody_visible = f => f}) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+        set_cardBody_visible(false);
+    };
+    const onCloseModal = () => {
+        cards.setSearchedPhrase("");
+        setIsModalOpen(false);
+        set_cardBody_visible(true);
+    };
+    
     return (
-        <Modal title={title}
-               centered
-               width="600px"
-               style={{
-                   height: "-webkit-fill-available",
-                   transformOrigin: "85px 43px",
-                   overflow: "hidden"
-               }}
-               open={isOpen}
-               closable={true}
-               onCancel={closeModal}
-               footer={[
-                   <Button key="Close"
-                           onClick={closeModal}>
-                     Close
-                   </Button>
-               ]}>
-          { children }
-        </Modal>
+        <>
+          <Button type="primary"
+                  onClick={showModal}>
+            Browse all cards
+          </Button>
+          <Modal title={<CardsBrowserTitle cards={cards}/>}
+                 centered
+                 width="600px"
+                 style={{
+                     height: "-webkit-fill-available",
+                     transformOrigin: "85px 43px",
+                     overflow: "hidden"
+                 }}
+                 open={isModalOpen}
+                 closable={true}
+                 onCancel={onCloseModal}
+                 footer={[
+                     <Button key="Close"
+                             onClick={onCloseModal}>
+                       Close
+                     </Button>
+                 ]}>
+            <AllCardsBrowser set_cardBody_visible={set_cardBody_visible}
+                             cards={cards}/>
+          </Modal>
+        </>
     );
 }
 
